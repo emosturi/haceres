fc732ca41d6b29d2cba7da18be752cdcfbbbec66
emosturi <nicsalb@gmail.com>
2022-08-06 20:07:54 -0400

    Features - Now todos are sorted by time by default

--- haceres/src/components/todolist/TodoList.js
+++ haceres/src/components/todolist/TodoList.js
@@ -6,10 +6,16 @@ import TodoDetails from "../tododetails/TodoDetails";
 const TodoList = () => {
     const { todos } = useContext(TodoContext);
     const { isDarkTheme } = useContext(ThemeContext);
+
+    //This should become a customizable sort for client use
+    const todosSortedByDate = todos.sort(
+        (a, b) => a.time.replace(/:/g, "") - b.time.replace(/:/g, "")
+    );
+
     return todos.length ? (
         <div className={`Todo-list ${isDarkTheme ? "dark" : ""}`}>
             <ul>
-                {todos.map((todo) => (
+                {todosSortedByDate.map((todo) => (
                     <TodoDetails key={todo.id} todo={todo} />
                 ))}
             </ul>
