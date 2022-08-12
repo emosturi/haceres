1dc19f23a31b697cd368e0f6bc5a0d1a3027e7c1
emosturi <nicsalb@gmail.com>
2022-08-12 10:16:42 -0400

    Busines Logic - Added logic of handleNewTodos to main component & created handleSubmit func. inside todoForm

--- haceres/src/components/main/main.js
+++ haceres/src/components/main/main.js
@@ -1,7 +1,7 @@
 import { collection, getDocs } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { TodoContext } from "../../contexts/TodoContext";
+// import { TodoContext } from "../../contexts/TodoContext";
 import { auth, db } from "../../utils/firebase/firebase.utils";
 import TodoForm from "../todoform/TodoForm";
 import TodoList from "../todolist/TodoList";
@@ -13,21 +13,26 @@ const Main = () => {
     console.log(user);
     useEffect(() => {
         if (user) {
-            const arrayOfTodos = [];
-            const getTodos = async () => {
-                const response = await getDocs(todosRef);
-                response.docs.forEach((doc) => {
-                    arrayOfTodos.push({ ...doc.data() });
-                });
-                setTodos([...arrayOfTodos]);
-            };
-            getTodos();
+            handleNewTodos();
         }
     }, [user]);
 
+    const handleNewTodos = async () => {
+        const arrayOfTodos = [];
+        const response = await getDocs(todosRef);
+        response.docs.forEach((doc) => {
+            arrayOfTodos.push({ ...doc.data() });
+        });
+        setTodos([...arrayOfTodos]);
+    };
+
     return (
         <>
-            <TodoForm todos={todos} todosRef={todosRef} user={user} />
+            <TodoForm
+                handleNewTodos={handleNewTodos}
+                todosRef={todosRef}
+                user={user}
+            />
             <TodoList todos={todos} todosRef={todosRef} user={user} />
         </>
     );
--- haceres/src/components/todoform/TodoForm.js
+++ haceres/src/components/todoform/TodoForm.js
@@ -1,16 +1,29 @@
 import React, { useContext, useState } from "react";
 import { TodoContext } from "../../contexts/TodoContext";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { addDoc } from "firebase/firestore";
 
-const TodoForm = ({ todos, user, todosRef }) => {
+const TodoForm = ({ handleNewTodos, todosRef }) => {
     const [title, setTitle] = useState("");
     const [time, setTime] = useState("");
-    const { dispatch } = useContext(TodoContext);
+    // const { dispatch } = useContext(TodoContext);
     const { isDarkTheme } = useContext(ThemeContext);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        dispatch({ type: "ADD_Todo", todo: { time, title } });
+        // dispatch({ type: "ADD_Todo", todo: { time, title } });
+        console.log("entrada al query");
+        try {
+            const docRef = await addDoc(todosRef, {
+                title,
+                time,
+                day: "Sabado",
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+        handleNewTodos();
         setTitle("");
         setTime("");
     };
